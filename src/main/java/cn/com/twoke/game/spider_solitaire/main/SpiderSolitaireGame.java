bbdaf389245f9bc5d38161f17f55e7f31546dbd3
TwoKe <twoke945@163.com>
2024-02-23 16:57:28 +0800

    feat: 添加拖牌放置校验，必须为连续时才能放置

--- src/main/java/cn/com/twoke/game/spider_solitaire/main/SpiderSolitaireGame.java
+++ src/main/java/cn/com/twoke/game/spider_solitaire/main/SpiderSolitaireGame.java
@@ -10,6 +10,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import javax.imageio.metadata.IIOMetadataFormat;
+
 import cn.com.twoke.game.spider_solitaire.constant.ImageResource;
 import cn.com.twoke.game.spider_solitaire.entity.Poker;
 import cn.com.twoke.game.spider_solitaire.entity.PokerStack;
@@ -241,8 +243,9 @@ public class SpiderSolitaireGame extends Game implements MouseListener, MouseMot
 	private boolean doCheckAndPickUpPoker(int stackIndex, int mouseX, int mouseY, PokerStack stack) {
 		int stackSize  = stack.size();
 		for (int i = stackSize - 1; i >= 0; i--) {
+			if(!stack.get(i).isTurnOver()) continue;
 			Rectangle pressedHitbox = new Rectangle(stack.getLastPokerX(),
-					stack.getLastPokerY() - (stackSize - 1 - i) * TURN_OFFSET, POKER_WIDTH, POKER_HEIGHT);
+					stack.getLastPokerY() - (stackSize - 1 - i) * TURN_OFFSET, POKER_WIDTH, stackSize - 1 == i ? POKER_HEIGHT : TURN_OFFSET);
 			if (pressedHitbox.contains(mouseX, mouseY)) {
 				this.pressedHitbox = pressedHitbox;
 				for (int k = i; k < stackSize; k++) {
@@ -278,7 +281,7 @@ public class SpiderSolitaireGame extends Game implements MouseListener, MouseMot
 	private void doPlacePokers() {
 		for (int i = 0; i < pokerStacks.length; i++) {
 			PokerStack stack =  pokerStacks[i];
-			if (stack.intersects(draggedPokerHitbox)) { // 判断是否移动到另外的牌堆
+			if (stack.intersects(draggedPokerHitbox) && canPlace(stack, draggedPokers)) { // 判断是否移动到另外的牌堆
 				for (int j = 0; j < draggedPokers.size(); j++) { // 循环放置扑克到另外的牌堆
 					Poker poker = pokerStacks[draggedStackIndex].remove(draggedStartItemIndex);
 					stack.add(poker);
@@ -292,6 +295,13 @@ public class SpiderSolitaireGame extends Game implements MouseListener, MouseMot
 	}
 
 	
+	private boolean canPlace(PokerStack targetStack, List<Poker> draggedStack) {
+		if (targetStack.size() == 0) return true;
+		Poker draggedFirstPoker = draggedStack.get(0);
+		Poker targetLastPoker =   targetStack.get(targetStack.size() - 1);
+		return targetLastPoker.getNo().getId() - draggedFirstPoker.getNo().getId() == 1;
+	}
+
 	/**
 	 * 发牌操作
 	 */
